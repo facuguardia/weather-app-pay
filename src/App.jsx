ba2354d8f78fffa611b97b63029df504b275e407
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2023-04-24 18:14:34 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="bg-gradient-to-r from-slate-900 to-slate-700 w-full h-screen overflow-hidden flex flex-col justify-between p-2">
-      <div className="text-white flex flex-col justify-between md:justify-center w-full h-screen pl-3">
+    <div className="bg-gradient-to-r from-slate-900 to-slate-700 w-full h-screen overflow-hidden flex flex-col justify-between p-4">
+      <div className="text-white flex flex-col justify-between md:justify-center w-full h-screen">
         <WeatherPanel />
       </div>
       <Footer />
